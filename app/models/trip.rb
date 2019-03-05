bb599825a95bcf2ed6baf30ed298ad19a567bddc
sophie spratley <sophie.spratley@yahoo.co.uk>
2019-03-05 12:33:38 +0000

    controller for trips created and routes added. Validations added to model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :group
   belongs_to :user
+  validates :description, :destination, presence: true
+  validates :price, presence: true, only_integer: true
 end
