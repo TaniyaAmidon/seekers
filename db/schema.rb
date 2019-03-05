4ce29e38a3d1c5e4a841362edfcb4e3cda3af510
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-03-05 10:43:33 +0000

    groups model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_180921) do
+ActiveRecord::Schema.define(version: 2019_03_05_104213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
