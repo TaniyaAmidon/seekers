2f894571ed9ae6a9c1a1e868d2d3fcc7d169d5cc
Dan Vernon <danvernon@gmail.com>
2019-03-05 17:16:47 +0000

    added user_activities seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,6 @@ User.destroy_all
 
 puts 'starting seed...'
 
-puts 'seeding users...'
-
-locations = %w(Swindon Barrow-in-Furness Darlington Dewsbury Frampton Thamesmead Penge Haringey Littlehampton Salford Smethwick Cathays Totnes Leith Norwich Diss Taunton Gravesend Oldham Sunderland Southend-on-Sea Harlow Staines Belper Bridgend Omagh Sanquhar Wrexham)
-greeting = %w(Hello Hi Hey)
-
-50.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  dob = Faker::Date.between(65.years.ago, 20.years.ago)
-  location = "#{locations.sample}, UK"
-  User.create!(first_name: first_name, last_name: last_name, email: "#{first_name}.#{last_name}@gmail.com", password: "password", dob: dob, location: location, bio: "#{greeting.sample} my name's #{first_name}! I come from #{location}. My day-job is #{Faker::Job.seniority.downcase} #{Faker::Job.employment_type.downcase} #{Faker::Job.title.downcase} in #{Faker::Job.field.downcase} but my friends know my real passion is ADVENTURE.")
-end
-
-puts "seeding users completed - #{User.count} users in database"
-
-
-#seed activities
 puts 'creating activities'
 
 
@@ -41,6 +24,33 @@ activities = Activity.create([
 { name: 'Skiing' }])
 
 
+puts 'seeding users...'
 
+locations = %w(Swindon Barrow-in-Furness Darlington Dewsbury Frampton Thamesmead Penge Haringey Littlehampton Salford Smethwick Cathays Totnes Leith Norwich Diss Taunton Gravesend Oldham Sunderland Southend-on-Sea Harlow Staines Belper Bridgend Omagh Sanquhar Wrexham)
+greeting = %w(Hello Hi Hey)
+
+50.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  dob = Faker::Date.between(65.years.ago, 20.years.ago)
+  location = "#{locations.sample}, UK"
+  User.create!(first_name: first_name, last_name: last_name, email: "#{first_name}.#{last_name}@gmail.com", password: "password", dob: dob, location: location, bio: "#{greeting.sample} my name's #{first_name}! I come from #{location}. My day-job is #{Faker::Job.seniority.downcase} #{Faker::Job.employment_type.downcase} #{Faker::Job.title.downcase} in #{Faker::Job.field.downcase} but my friends know my real passion is ADVENTURE.")
+end
+
+puts "seeding users completed - #{User.count} users in database"
+
+puts "seeding user activities (2-4 activities per user)"
+
+User.count.times do
+  i = User.first.id
+  user = User.find(i)
+  rand(2..4).times do
+    act = Activity.all.sample
+    UserActivity.create!(user: user, activity: act)
+  end
+  i += 1
+end
+
+puts "user activities seeded"
 
 puts 'Seed done'
