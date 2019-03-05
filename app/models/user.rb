14a052866b150c40d5960ff73a83329afbcac19e
Dan Vernon <danvernon@gmail.com>
2019-03-05 15:33:08 +0000

    set dependent destroy on user_activities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :user_activities
-  has_many :activities, through: :user_activity
+  has_many :user_activities, dependent: :destroy
+  has_many :activities, through: :user_activities
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
