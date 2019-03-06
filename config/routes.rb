61212e01353e5b1cd270129d926c96243f87c825
Dan Vernon <danvernon@gmail.com>
2019-03-06 17:46:54 +0000

    configured forestadmin dashboard in test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
