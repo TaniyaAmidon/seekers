b00df91b24907fe95553db45b007a429b15b7daa
Dan Vernon <danvernon@gmail.com>
2019-03-06 17:16:24 +0000

    added title presence:true validation to model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,6 @@ class Trip < ApplicationRecord
   belongs_to :group
   belongs_to :user
   belongs_to :activity
-  validates :description, :destination, presence: true
+  validates :title, :description, :destination, presence: true
   monetize :price_cents
 end
