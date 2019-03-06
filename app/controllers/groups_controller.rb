340533deb5598562dedb0eb04e8cf777bdaf74bc
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-03-06 13:19:49 +0000

    show content added, link to sign up page added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,15 @@
-class GroupsController < ApplicationController
-  def new
-    @group = Group.new
-  end
+# class GroupsController < ApplicationController
+#   def new
+#     @group = Group.new
+#   end
 
-  def create
-    @group = Group.find(params[:id])
-    @group = Group.create
-  end
+#   def create
+#     @group = Group.find(params[:id])
+#     @group = Group.create
+#   end
 
-  def destroy
-    @group = Group.find(params[:id])
-    @group = Group.destroy
-  end
-end
+#   def destroy
+#     @group = Group.find(params[:id])
+#     @group = Group.destroy
+#   end
+# end
