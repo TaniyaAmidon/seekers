85ae84500d154366f980441a51747bee0cd38853
sophie spratley <sophie.spratley@yahoo.co.uk>
2019-03-07 19:55:36 +0000

    Revert "fuck cloudinary"
    This reverts commit 95d12572b0cb2221c12cc505702286da60d3cb7b.

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
 gem 'algoliasearch-rails'
 
+
 group :development do
   gem 'better_errors', '~> 2.5', '>= 2.5.1'
   gem 'binding_of_caller'
--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2019_03_07_183526) do
     t.integer "price_cents", default: 0, null: false
     t.integer "activity_id"
     t.string "title"
-    t.date "trip_date"
-    t.integer "days"
+    t.float "latitude"
+    t.float "longitude"
     t.date "start_date"
     t.date "end_date"
     t.text "organiser_exp"
@@ -69,8 +69,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_183526) do
     t.boolean "crew_exp_required"
     t.integer "max_crew_size"
     t.string "photo"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["group_id"], name: "index_trips_on_group_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
