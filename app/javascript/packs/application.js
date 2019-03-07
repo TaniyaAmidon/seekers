e85e2a28259c83a5ad667ab3632840ac7e390487
sophie spratley <sophie.spratley@yahoo.co.uk>
2019-03-07 13:09:56 +0000

    added algolia places js package and function to application.js. added autocomplete to new trip form input

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+import places from 'places.js';
+
+
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('autocomplete-destination');
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+};
+
+initAutocomplete();
