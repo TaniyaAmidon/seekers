2e303f8fd1c9c9c249cbcf9c218f59efa967d982
Dan Vernon <danvernon@gmail.com>
2019-03-07 18:38:15 +0000

    updated trip and user model to enable geocoding

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,6 @@ class Trip < ApplicationRecord
   belongs_to :activity
   validates :title, :description, :destination, presence: true
   monetize :price_cents
+  geocoded_by :destination
+  after_validation :geocode, if: :will_save_change_to_destination?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_121448) do
+ActiveRecord::Schema.define(version: 2019_03_07_183526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_03_07_121448) do
     t.integer "price_cents", default: 0, null: false
     t.integer "activity_id"
     t.string "title"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["group_id"], name: "index_trips_on_group_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
@@ -74,6 +76,8 @@ ActiveRecord::Schema.define(version: 2019_03_07_121448) do
     t.date "dob"
     t.text "bio"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
