9d668dc1658e90478ecad639b41780339b20ea71
Dan Vernon <danvernon@gmail.com>
2019-03-07 16:08:59 +0000

    added inbuilt materials_green stlying to picker form

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,5 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/material_blue.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 // flatpickr(".datepicker", {
