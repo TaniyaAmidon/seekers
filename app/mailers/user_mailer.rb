8544d10fa206012caadf32e3b3072e9937484398
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-07 17:47:46 +0000

    adding response to applucation to organiser

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,11 @@ class UserMailer < ApplicationMailer
     @greeting = "Hi"
     mail(to: member.user.email, subject: 'Thanks for your application')
   end
+
+  def respond_to_application(member)
+    group = member.group.id
+    trip = Trip.where(group_id: group)
+    organiser = trip.user
+    mail(to: organiser.email, subject: 'You have an application to join!')
+  end
 end
