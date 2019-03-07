342f1a9cb536a9eb97b7ad5a75fae6f696b7bba1
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-07 17:13:12 +0000

    changed env details

--- config/initializers/smpt.rb
+++ config/initializers/smpt.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: 587,
   domain: 'gmail.com',
   user_name: 'seekrs.fun@gmail.com',
-  password: 'seekadventure',
+  password: 'cghjkonfisnyziri',
   authentication: :login,
   enable_starttls_auto: true
 }
