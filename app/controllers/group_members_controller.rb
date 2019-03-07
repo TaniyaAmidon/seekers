ea62919898767fde5fa3fce5502ab06cff26e414
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-07 14:42:51 +0000

    created index of users for certain trip

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,5 +1,6 @@
 class GroupMembersController < ApplicationController
-  before_action :find_group_member, only: [:show, :reject, :accept, :destroy]
+  before_action :find_group_member, only: [:show, :reject, :accept, :destroy, :pending_index, :rejected_index, :accepted_index]
+  before_action :find_trip, only: [:pending_index, :rejected_index, :accepted_index, :index]
 
   def new
     @trip = Trip.find(params[:trip_id])
@@ -17,32 +18,39 @@ class GroupMembersController < ApplicationController
 
   def destroy
     @group_member.destroy
-    # redirect_to (group_member inder show)
   end
 
-  def reject
-    @group_member.status = "rejected"
-  end
+  # def reject
+  #   @group_member.status = "rejected"
+  # end
 
-  def accept
-    @group_member.status = "accepted"
+  # def accept
+  #   @group_member.status = "accepted"
+  # end
+  def index
+    @group_members = GroupMember.where(group: @trip.group)
   end
 
   def pending_index
-    @pending_members = GroupMember.where(status: "pending")
+    @pending_members = GroupMember.where(status: "pending", group: @trip.group)
   end
+  helper_method :pending_index
 
   def accepted_index
-    @accepted_members = GroupMember.where(status: "accepted")
+    @accepted_members = GroupMember.where(status: "accepted", group: @trip.group)
   end
+  helper_method :accepted_index
 
   def rejected_index
-    @rejected_members = GroupMember.where(status: "rejected")
+    @rejected_members = GroupMember.where(status: "rejected", group: @trip.group)
   end
-
-  def
+  helper_method :rejected_index
 
   private
+
+  def find_trip
+    @trip = Trip.find(params[:trip_id])
+  end
 
   def find_group_member
     @group_member = GroupMember.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   resources :trips do
-    resources :group_members, only: [:new, :create, :edit, :update, :destroy]
+    resources :group_members, only: [:new, :create, :destroy, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
