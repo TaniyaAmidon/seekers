1c2b4043922b0cb4d6c88f0ac3c741498d99db45
Dan Vernon <danvernon@gmail.com>
2019-03-11 11:45:35 +0000

    added logic to showpage to display deposit payment modal only if user has not paid

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def create
     trip = Trip.find(params[:trip_id])
-    order = Order.create!(trip: trip, amount: trip.price, state: 'pending', user: current_user)
+    order = Order.create!(trip: trip, amount: (trip.price/10), state: 'pending', user: current_user)
 
     redirect_to new_order_payment_path(order)
   end
