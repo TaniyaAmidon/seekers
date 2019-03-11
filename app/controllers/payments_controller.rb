b403995e0f6c9720c2d84386d08e51895c14a51d
Dan Vernon <danvernon@gmail.com>
2019-03-11 10:39:40 +0000

    payment create method populated with stripe actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,23 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    # ...
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
+
+    charge = Stripe::Charge.create(
+      customer:     customer.id,
+      amount:       @order.amount_cents,
+      description:  "Deposit payment for #{@order.trip.id} for order #{@order.id}",
+      currency:     @order.amount.currency
+    )
+
+    @order.update(payment: charge.to_json, state: 'paid')
+    redirect_to order_path(@order)
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to new_order_payment_path(@order)
   end
 
   private
