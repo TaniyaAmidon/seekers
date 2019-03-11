7808982b9c205459a01c8e09f4b7d09e973af8ac
Dan Vernon <danvernon@gmail.com>
2019-03-11 10:33:03 +0000

    added basic create method to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+  def create
+    trip = Trip.find(params[:trip_id])
+    order = Order.create!(trip: trip.id, amount: trip.price, state: 'pending', user: current_user)
+
+    redirect_to new_order_payment_path(order)
+  end
 end
